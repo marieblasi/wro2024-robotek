d2c09186c1eaddfe16bc558c5787b08b244a82b2
isabella <isabellamilagros842@gmail.com>
2024-08-18 17:03:08 +0100

    Added a code using forward and stop functions from motor.py

--- src/main.py
+++ src/main.py
@@ -1,26 +1,18 @@
-import cv2
-from PIL import Image
-from util import get_limits
-#info de la webcam
-cap = cv2.VideoCapture(1)
-
-red = [40, 20, 150]
-blue = [30, 20, 150]
+from motor import forward, backwards, stop
+from ser import read_sensors
 
 while True:
-    ret, frame = cap.read()
-    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lowerLimit, upperLimit = get_limits(color=red)
-    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-    mask_ = Image.fromarray(mask)
-    bbox = mask_.getbbox()
+    sensor_values = read_sensors()
+    
+    if sensor_values:
+        sensor1, sensor2, sensor3 = sensor_values
+        print(sensor1, sensor2, sensor3)
+        
+        if sensor2 > 18:
+            forward()
+        else:
+            stop()
+        
 
-    if bbox is not None:
-        x1, y1, x2, y2 = bbox
-        frameWithBbox = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-        cv2.imshow("frame", frameWithBbox)
-    else:
-        cv2.imshow("frame", frame)
+    
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
