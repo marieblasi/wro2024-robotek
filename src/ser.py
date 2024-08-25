6b5b3509d011b43010430e07afd6d90a7d79b571
Robotek <isabellamilagros842@gmail.com>
2024-08-25 17:59:30 +0100

    add buffer to retrieve last line of the readline function from serial

--- src/ser.py
+++ src/ser.py
@@ -2,15 +2,23 @@ import serial
 
 ser = serial.Serial("/dev/ttyUSB0", 9600, timeout = 1)
 
+buffer_string = ''
+
 def read_sensors():
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            values = line.split(',')
-            if len(values) == 3:
-                sensor1, sensor2, sensor3 = map(int, values)
-                #print(f"Sensor 1: {sensor1}, Sensor 2: {sensor2}, Sensor 3: {sensor3}")
-                return sensor1, sensor2, sensor3
-            else:
-                print("Received incomplete data")
-                return None
-        return None
+    global buffer_string
+    global last_received
+    buffer_string = buffer_string + ser.read(ser.inWaiting()).decode('utf-8')
+    if '\n' in buffer_string:
+        lines = buffer_string.split('\n')
+        last_received = lines[-2]
+        buffer_string = lines[-1]
+        line = last_received.rstrip()
+        values = line.split(',')
+        if len(values) == 3:
+            sensor1, sensor2, sensor3 = map(int, values)
+            #print(f"Sensor 1: {sensor1}, Sensor 2: {sensor2}, Sensor 3: {sensor3}")
+            return sensor1, sensor2, sensor3
+        else:
+            print("Received incomplete data")
+            return None
+    return None
