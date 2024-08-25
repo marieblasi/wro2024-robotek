e6dd8af6aaaf8f603f44e8e746f1edafedd75026
Robotek <isabellamilagros842@gmail.com>
2024-08-25 20:41:30 +0100

    add PID logic function and steering rotation function (based on PID values) for turnings

--- src/main.py
+++ src/main.py
@@ -1,27 +1,41 @@
 from motor import forward, backwards, stop, left, right
 from ser import read_sensors
-from time import sleep
+from time import sleep, time
+
+kp = 0.8
+kd = 0.01
+ki = 0.01
+
+previous_error = 0 
+last_time = time()
+integral = 0
+
+def pid_control(d_left, d_right):
+    global previous_error, last_time, integral
+    current_time = time()
+    e = d_left - d_right
+    P = kp * e
+    dt = current_time - last_time
+    integral = integral + (e * dt)
+    derivative = (e - previous_error) / dt
+    I = ki * integral
+    D = kd * derivative
+    print(f'P: {P}; I: {I}; D: {D}')
+
+    previous_error = e
+    last_time = current_time
+    return int(P + I + D) 
 
 steering_angle = 0 
-# -200 to 200
 while True:
     sensor_values = read_sensors()
     
     if sensor_values:
         sensor1, sensor2, sensor3 = sensor_values
         print(sensor1, sensor3)
-        dx = sensor3 - sensor1
-        if ( abs(dx) > 10):
-            if (dx > 0):
-                left(20)
-                # steering_angle = steering_angle + (-100)
-            if (dx < 0):
-                right(10)
-        
-        # if sensor2 > 18:
-        #     forward()
-        # else:
-        #     stop()
-        sleep(0.1)
-    
-
+        steering_change = pid_control(sensor1, sensor3)
+        print(f'steering change : {steering_change}')
+        if (steering_change < 0):
+            left(abs(20))
+        elif (steering_change > 0):
+            right(abs(20))
