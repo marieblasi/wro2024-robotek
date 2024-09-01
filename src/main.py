a29c9c1dfb2dd4e9e874e3fe02f3bd0749ce0943
isabella <isabellamilagros842@gmail.com>
2024-09-01 21:18:27 +0100

    add limit to PID and let it read all the sensors

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from motor import forward, backwards, stop, left, right
 from ser import read_sensors
 from time import sleep, time
 
-kp = 0.8
+kp = 2.4
 kd = 0.01
 ki = 0.01
 
@@ -24,18 +24,22 @@ def pid_control(d_left, d_right):
 
     previous_error = e
     last_time = current_time
-    return int(P + I + D) 
+    return min(int(P + I + D), 100)
 
 steering_angle = 0 
 while True:
     sensor_values = read_sensors()
     
     if sensor_values:
-        sensor1, sensor2, sensor3 = sensor_values
-        print(sensor1, sensor3)
-        steering_change = pid_control(sensor1, sensor3)
+        dist_l, dist_t, dist_r, qtr_L, qtr_R = sensor_values
+        print(dist_l, dist_r)
+        steering_change = pid_control(dist_l, dist_r)
         print(f'steering change : {steering_change}')
         if (steering_change < 0):
-            left(abs(20))
+            left(abs(steering_change))
         elif (steering_change > 0):
-            right(abs(20))
+            right(abs(steering_change))
+        if (dist_t < 15):
+            stop()
+        # else:
+        #     forward(0.6)
